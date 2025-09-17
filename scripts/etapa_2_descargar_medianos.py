5d79c67bf26dba89898f7d7fedec88c8fb5414d6
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-09-16 20:07:46 -0500

    Update etapa_2_descargar_medianos.py

--- scripts/etapa_2_descargar_medianos.py
+++ scripts/etapa_2_descargar_medianos.py
@@ -35,9 +35,18 @@ def estadisticas_completas(estadisticas: dict) -> bool:
 
 def extraer_estadisticas_red(url_php):
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "text/html"
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/115.0.0.0 Safari/537.36"
+        ),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
     }
+
     response = requests.get(url_php, headers=headers, timeout=10)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "html.parser")
