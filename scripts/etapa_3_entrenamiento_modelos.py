06205be264bf37df412b93a7bed86413b2080b5a
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-09-20 22:17:25 -0500

    Update etapa_3_entrenamiento_modelos.py

--- scripts/etapa_3_entrenamiento_modelos.py
+++ scripts/etapa_3_entrenamiento_modelos.py
@@ -125,7 +125,7 @@ for model_name, model in models.items():
 # Paso 5: TPOT (modelo genético)
 # -----------------------------
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-tpot = TPOTRegressor(generations=5, population_size=20, random_state=42, verbosity=2)
+tpot = TPOTRegressor(generations=5, population_size=20, random_state=42)
 tpot.fit(X_train, y_train)
 
 best_model = tpot.fitted_pipeline_
